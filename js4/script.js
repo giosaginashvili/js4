f48060799620a740e3014d08188889452b24cfef
giosaginashvili <164656337+giosaginashvili@users.noreply.github.com>
2024-06-20 03:11:13 -0700

    Add files via upload

---
+++ js4/script.js
@@ -0,0 +1,23 @@
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+
+let even = [];
+for(let a = 0; a < numbers.length; a++) {
+       if (numbers[a] % 2 == 0)
+       even.push(numbers[a]);
+}
+
+console.log(`Even numbers in an array are: ${even}`);
+
+
+
+
+
+
+
+
+
+
+const arr = [1, 2, 3, 4, 5,6,7,8,9,10];
+ let sum = 0; for (let i = 0; i < arr. length; i++) { sum += arr[i]; } console. log(sum);
